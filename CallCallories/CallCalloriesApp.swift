d7309104bda1e9d551e7ac65a5446a431620ae31
irvan <pradaindo24@gmail.com>
2023-08-07 13:28:34 +0800

    Initial Commit

---
+++ CallCallories/CallCalloriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CallCalloriesApp.swift
+//  CallCallories
+//
+//  Created by Irvan P. Saragi on 07/08/23.
+//
+
+import SwiftUI
+
+@main
+struct CallCalloriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
